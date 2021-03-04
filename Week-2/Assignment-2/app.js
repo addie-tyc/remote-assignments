9e2fd5ccfe556bfaaf8383053a292c46b399f446
Tsai-Yun, Chung <65952631+addie-tyc@users.noreply.github.com>
2021-03-04 16:18:26 +0800

    Update new button

--- Week-2/Assignment-2/app.js
+++ Week-2/Assignment-2/app.js
@@ -1,14 +1,67 @@
 const heading = document.querySelector("h1");
 const button = document.querySelector("#button");
 const moreBox = document.querySelectorAll(".moreBox");
+const hide = document.querySelector(".hide");
+const downArrow = document.querySelector("#downArrow");
+const upArrow = document.querySelector("#upArrow");
 
 heading.addEventListener("click", () => {
-  heading.textContent = "Have a Good Time!";
-})
-
-button.addEventListener("click", () => {
-  for (var i = 0; i < moreBox.length; i++){
-    moreBox[i].style.display='inline-block';
+  if (heading.textContent == "Have a Good Time!"){
+    heading.textContent = "Welcome Message";
+  } else {
+    heading.textContent = "Have a Good Time!";
   }
 })
 
+button.addEventListener("click", () => {
+  if (moreBox[0].style.display == 'inline-block') {
+    for (var i = 0; i < moreBox.length; i++){
+      moreBox[i].style.display='none';
+    }
+    upArrow.style.display = "none";
+  } else {
+    for (var i = 0; i < moreBox.length; i++){
+      moreBox[i].style.display='inline-block';
+    }
+    downArrow.style.display = "none";
+  }
+})
+
+
+// button.addEventListener("click", () => {
+//   if (moreBox[0].style.display == 'none') {
+//     for (var i = 0; i < moreBox.length; i++){
+//       moreBox[i].style.display='inline-block';
+//     }
+//     button.textContent = " \u25b2 ";
+//   } else {
+//     for (var i = 0; i < moreBox.length; i++){
+//       moreBox[i].style.display='none';
+//     }
+//     button.textContent = "Call to Action \u25bc";
+//   }
+// })
+
+// use event instead of hover in CSS
+
+button.addEventListener("mouseenter", () => {
+  button.style.backgroundColor = "rgb(159, 198, 236)";
+  button.style.color = "white";
+  if (moreBox[0].style.display == 'inline-block') {
+    upArrow.style.display = "inline";
+    downArrow.style.display = "none";
+  } else {
+    downArrow.style.display = "inline";
+    upArrow.style.display = "none";
+  }
+})
+
+button.addEventListener("mouseleave", () => {
+  button.style.backgroundColor = "rgb(204, 220, 236)";
+  button.style.color = "black";
+  if (moreBox[0].style.display == 'inline-block') {
+    upArrow.style.display = "none";
+  } else {
+    downArrow.style.display = "none";
+  }
+})
